c78f442518829f45470d4c935994b3a9d9b26fb1
isaachegg <isaachegg@gmail.com>
2023-09-28 22:53:14 -0500

    orignial commit

---
+++ main.py
@@ -0,0 +1,175 @@
+import chess
+import chess.svg 
+import random
+
+
+board = chess.Board()
+Element('py').write(board)
+
+
+def click(*args):
+    result = Element('output')
+    move = Element('move').value
+    try:
+        turn(move)
+        Element('output').write("Enter Next Move")
+    except:
+        Element('output').write("Not Valid Move. Try Again.")
+
+
+def botMove(board):
+    bestMove = chess.Move.null()
+    bestValue = -99999
+    alpha = -100000
+    beta = 100000
+    for move in board.legal_moves:
+        board.push(move)
+        boardValue = -minimax(-alpha, -beta, 3)
+        if boardValue > bestValue:
+            bestValue = boardValue
+            bestMove = move
+        if boardValue > alpha:
+            alpha = boardValue
+        board.pop()
+    return bestMove
+   
+   
+    # if board.turn is False:
+    #     legalMoves = list(board.legal_moves)
+    #     return legalMoves[int(random.random() * len(legalMoves))]
+
+def turn(move):
+    board.push_san(move)
+    if board.is_game_over():
+        return
+    board.push_san(str(botMove(board)))
+    Element('py').write(board)
+    print(evaluation())
+    
+
+def evaluation():
+    pawntable = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    5, 10, 10, -20, -20, 10, 10, 5,
+    5, -5, -10, 0, 0, -10, -5, 5,
+    0, 0, 0, 20, 20, 0, 0, 0,
+    5, 5, 10, 25, 25, 10, 5, 5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    0, 0, 0, 0, 0, 0, 0, 0]
+
+    knightstable = [
+        -50, -40, -30, -30, -30, -30, -40, -50,
+        -40, -20, 0, 5, 5, 0, -20, -40,
+        -30, 5, 10, 15, 15, 10, 5, -30,
+        -30, 0, 15, 20, 20, 15, 0, -30,
+        -30, 5, 15, 20, 20, 15, 5, -30,
+        -30, 0, 10, 15, 15, 10, 0, -30,
+        -40, -20, 0, 0, 0, 0, -20, -40,
+        -50, -40, -30, -30, -30, -30, -40, -50]
+    bishopstable = [
+        -20, -10, -10, -10, -10, -10, -10, -20,
+        -10, 5, 0, 0, 0, 0, 5, -10,
+        -10, 10, 10, 10, 10, 10, 10, -10,
+        -10, 0, 10, 10, 10, 10, 0, -10,
+        -10, 5, 5, 10, 10, 5, 5, -10,
+        -10, 0, 5, 10, 10, 5, 0, -10,
+        -10, 0, 0, 0, 0, 0, 0, -10,
+        -20, -10, -10, -10, -10, -10, -10, -20]
+    rookstable = [
+        0, 0, 0, 5, 5, 0, 0, 0,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        -5, 0, 0, 0, 0, 0, 0, -5,
+        5, 10, 10, 10, 10, 10, 10, 5,
+        0, 0, 0, 0, 0, 0, 0, 0]
+    queenstable = [
+        -20, -10, -10, -5, -5, -10, -10, -20,
+        -10, 0, 0, 0, 0, 0, 0, -10,
+        -10, 5, 5, 5, 5, 5, 0, -10,
+        0, 0, 5, 5, 5, 5, 0, -5,
+        -5, 0, 5, 5, 5, 5, 0, -5,
+        -10, 0, 5, 5, 5, 5, 0, -10,
+        -10, 0, 0, 0, 0, 0, 0, -10,
+        -20, -10, -10, -5, -5, -10, -10, -20]
+    kingstable = [
+        20, 30, 10, 0, 0, 10, 30, 20,
+        20, 20, 0, 0, 0, 0, 20, 20,
+        -10, -20, -20, -20, -20, -20, -20, -10,
+        -20, -30, -30, -40, -40, -30, -30, -20,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30,
+        -30, -40, -40, -50, -50, -40, -40, -30]
+    
+    if board.is_insufficient_material():
+        return 0
+
+    wp = len(board.pieces(chess.PAWN, chess.WHITE))
+    bp = len(board.pieces(chess.PAWN, chess.BLACK))
+
+    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+
+    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+
+    wr = len(board.pieces(chess.ROOK, chess.WHITE))
+    br = len(board.pieces(chess.ROOK, chess.BLACK))
+
+    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+
+    material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
+    pawnsq = sum([pawntable[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+    pawnsq = pawnsq + sum([-pawntable[chess.square_mirror(i)]
+                        for i in board.pieces(chess.PAWN, chess.BLACK)])
+    knightsq = sum([knightstable[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+    knightsq = knightsq + sum([-knightstable[chess.square_mirror(i)]
+                            for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+    bishopsq = sum([bishopstable[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
+    bishopsq = bishopsq + sum([-bishopstable[chess.square_mirror(i)]
+                            for i in board.pieces(chess.BISHOP, chess.BLACK)])
+    rooksq = sum([rookstable[i] for i in board.pieces(chess.ROOK, chess.WHITE)])
+    rooksq = rooksq + sum([-rookstable[chess.square_mirror(i)]
+                        for i in board.pieces(chess.ROOK, chess.BLACK)])
+    queensq = sum([queenstable[i] for i in board.pieces(chess.QUEEN, chess.WHITE)])
+    queensq = queensq + sum([-queenstable[chess.square_mirror(i)]
+                            for i in board.pieces(chess.QUEEN, chess.BLACK)])
+    kingsq = sum([kingstable[i] for i in board.pieces(chess.KING, chess.WHITE)])
+    kingsq = kingsq + sum([-kingstable[chess.square_mirror(i)]
+                        for i in board.pieces(chess.KING, chess.BLACK)])
+
+    eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
+    if board.turn:
+        return eval
+    else:
+        return -eval
+
+
+
+def minimax(alpha, beta, depth):
+    bestEval = -1000000
+    if depth == 0 or board.is_game_over():
+        eval = evaluation()
+        if (eval >= beta):
+            return beta
+        if (alpha < eval):
+            alpha = eval
+        return eval
+    print("flag")
+    for move in board.legal_moves:
+        board.push(move)
+        eval = -minimax(-alpha, -beta, depth - 1)
+        board.pop()
+        if eval > bestEval:
+            bestEval = eval
+        if eval >= beta:
+            return eval
+        if eval > alpha:
+            alpha = eval
+    return bestEval
+    
+    
